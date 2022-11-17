b7521bc9bafc9a47bceeb81e75956f8916012be9
Jeremy McGowan <jeremymcgowan@hotmail.com>
2022-11-16 17:57:22 -0800

    Passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport')
+const authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,8 +38,7 @@ connect.then(()=> console.log('Connected correctly to server'),
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -51,6 +54,12 @@ app.use(session({
 
 
 }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req,res,next){
  if(!req.session.user){
@@ -91,18 +100,28 @@ function auth(req,res,next){
  }
 }
 
+
+
 app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+  } else {
+    
+      return next();
+      } 
+}
 
 app.use('/campsites',campsiteRouter);
 app.use('/promotions',promotionRouter);
 app.use('/partners',partnerRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
