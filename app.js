3a0cb5598a7db01c0560a849f9cbc1fda67388ef
Jeremy McGowan <jeremymcgowan@hotmail.com>
2022-11-17 17:35:35 -0800

    Passport JWT

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+
+
 const passport = require('passport')
-const authenticate = require('./authenticate')
+const config = require('./config')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,7 +20,7 @@ const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url,{
     useCreateIndex: true,
     useFindAndModify: false,
@@ -45,21 +44,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-   name: 'session-id',
-   secret: '12345-67890-09876-54321',
-   saveUninitialized: false,
-   resave: false,
-   store: new FileStore()
 
-
-}))
 
 app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/campsites',campsiteRouter);
+app.use('/promotions',promotionRouter);
+app.use('/partners',partnerRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 function auth(req,res,next){
  if(!req.session.user){
@@ -101,42 +115,5 @@ function auth(req,res,next){
 }
 
 
-
-app.use(auth)
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-  } else {
-    
-      return next();
-      } 
-}
-
-app.use('/campsites',campsiteRouter);
-app.use('/promotions',promotionRouter);
-app.use('/partners',partnerRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
